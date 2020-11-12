80f65c36a2f686d24c09d8a7f7a3fe64dd802ce1
Savahi <11804911+Savahi@users.noreply.github.com>
2020-11-12 13:52:34 +0300

    some work saved again

--- match/match_lib.py
+++ match/match_lib.py
@@ -33,66 +33,104 @@ skill_answers = {
 # Matches a pair of one question and one answer
 # Raises the "MatchError" custom exception if fails
 def match_pair(q, a):
-    if not 'weight' in q:
+    if not 'weight' in q:   # If weight is not specified - make it "1.0"
         weight = 1.0
     else:
-        weight = q.weight
+        weight = q['weight']
 
-    if q['type'] == Q_YN:
-        return (q.value - a.value) * weight
-    if q['type'] == Q_GT:
-        diff = q.value - a.value
+    if q['type'] == Q_YN:                       # A "yes-no" question
+        return (q['value'] - a['value']) * weight
+    if q['type'] == Q_GT:                       # A "greater than" one
+        diff = q['value'] - a['value']
         if not (diff > 0):
             return 0
         return diff * weight
-    if q['type'] == Q_EQ:
-        return abs(q.value - a.value) * weight
+    if q['type'] == Q_EQ:                       # An "equal to" one
+        return abs(q['value'] - a['value']) * weight
     raise MatchError
 
 
-# Matches an array of questions with an array of answers, both arrays must be of the same size
+# Matches an array-like of questions with a dictionary of answers
+# "id" of a question matches the "key" of an answer, e.g. q = {'id': 1, ...} => a = { 1: {}, ... }
 # Raises the "MatchError" custom exception if fails
-# Input: "questions" - array-like; "anwsers" - array-like; "method" - matching method (by now it is a number to power the matching error)
+# Input: "questions" - array-like; "anwsers" - dictionary; 
+# "method" - matching method (by now it is a number to power the matching error up to)
 def match_questions(questions, answers, method=2):
+    if method < 1:
+        raise MatchError
     matching_errors_sum = 0.0
-    weight1_matching_errors_sum = 0.0
+    must_have_errors_sum = 0.0
     for q in questions:
         if not (q['id'] in answers):    # If there is no answer to a question...
             a = { 'value': 0.0 }
         else:
-            a = answers[ a['id'] ]
+            a = answers[ q['id'] ]
                   
-        matching_error = match_pair(q, a)
-        matching_errors_sum += matching_error**method
-        if not q.weight < 1.0:
-            weight1_matching_errors_sum += matching_error 
+        e = match_pair(q, a)
+        matching_errors_sum += e**method
+        if not q['weight'] < 1.0:
+            must_have_errors_sum += e**method 
 
-    return matching_errors_sum, weight1_matching_errors_sum
-                    
+    error = matching_errors_sum**(1.0/method) if matching_errors_sum > 0.0 else 0.0
+    must_have_error = must_have_errors_sum**(1.0/method) if must_have_errors_sum > 0.0 else 0.0
 
+    return { 
+        'error': error, 
+        'must_have_error': must_have_error 
+    } 
+# end of match_questions                    
+
+# Matches an array-like of questions with a dictionary of answers
+# "id" of a question matches the "key" of an answer, e.g. q = {'id': 1, ...} => a = { 1: {}, ... }
+# Raises the "MatchError" custom exception if fails
+# Input: "questions" - array-like; "anwsers" - dictionary; 
+# "method" - matching method (by now it is a number to power the matching error up to)
 def match_skills(skill_questions, skill_answers, method=2):
-    skill_matching_errors_sum = 0.0
+    if method < 1:
+        raise MatchError
+    skill_errors_sum = 0.0
+    overskill_errors_sum = 0.0
     wish_to_learn_errors_sum = 0.0
     wish_to_engage_errors_sum = 0.0;
     for sq in skill_questions:
-        q = { 'type': Q_GT, 'value': s['skill_level'] }
-        id = q['id']
-        a = s[id].skill_level if id in skill_answers else 0.0
-        error = match_pair(q, a)
-        skill_matching_errors_sum += error**method
+        id = sq['id']
+        # Calculating skill error
+        q = { 'type': Q_GT, 'value': sq['skill_level'] }
+        v = skill_answers[id]['skill_level'] if id in skill_answers else 0.0
+        error = match_pair(q, {'value': v})
+        skill_errors_sum += error**method
+        
+        q['type'] = Q_EQ    # Calculating overskill
+        error = match_pair(q, {'value': v})
+        if error < 0:
+            overskill_errors_sum += error**method
 
-        q = { 'type': Q_GT, 'value': s['wish_to_learn'] }
-        id = q['id']
-        a = s[id].wish_to_learn if id in skill_answers else 0.0
-        error = match_pair(q, a)
+        # Calculating "wish to learn" error
+        q = { 'type': Q_GT, 'value': sq['wish_to_learn'] }
+        v = skill_answers[id]['wish_to_learn'] if id in skill_answers else 0.0
+        error = match_pair(q, {'value':v} )
         wish_to_learn_errors_sum += error**method
 
-        q = { 'type': Q_GT, 'value': s['wish_to_engage'] }
-        id = q['id']
-        a = s[id].wish_to_engage if id in skill_answers else 0.0
-        error = match_pair(q, a)
+        # Calculating "wish_to_engage" error
+        q = { 'type': Q_GT, 'value': sq['wish_to_engage'] }
+        v = skill_answers[id]['wish_to_engage'] if id in skill_answers else 0.0
+        error = match_pair(q, {'value':v})
         wish_to_engage_errors_sum += error**method
 
-    return skill_matching_errors_sum, wish_to_learn_errors_sum, wish_to_engage_errors_sum
+        skill_error = skill_errors_sum**(1.0/method) if skill_errors_sum > 0.0 else 0.0 
+        overskill_error = overskill_errors_sum**(1.0/method) if overskill_errors_sum > 0.0 else 0.0 
+        wtl_error = wish_to_learn_errors_sum**(1.0/method) if wish_to_learn_errors_sum > 0.0 else 0.0 
+        wte_error = wish_to_engage_errors_sum**(1.0/method) if wish_to_engage_errors_sum > 0.0 else 0.0 
+
+    return { 
+        'skill_error': skill_error, 
+        'overskill_error': overskill_error,
+        'wish_to_learn_error': wtl_error, 
+        'wish_to_engage_error': wte_error
+    }
+#end of match_skills
 
 
+
+print( match_questions(questions, answers) )
+print( match_skills(skill_questions, skill_answers ) )
